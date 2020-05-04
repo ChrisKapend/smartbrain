205d8c13985dbe18bcb254f2613fb9b0b06b918d
Chris Kapend <ChrisKapend@gmail.com>
2020-05-04 17:03:00 +0200

    adding google cloud vision key

---
+++ smartbrain.js
@@ -0,0 +1,12 @@
+{
+  "type": "service_account",
+  "project_id": "smartbrain-276214",
+  "private_key_id": "0eb1455fe7f24a73b07800cfe2744bfd44e36816",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEugIBADANBgkqhkiG9w0BAQEFAASCBKQwggSgAgEAAoIBAQCQIxMlmCOZ0rgh\n8zZ0ZtRrB8iyFNwPV1irxcoZtlO+y2IWTpBvodTwfoopQg6EnOknUXjuHTlgmCyO\nvgsxZ/5cr52iKfNv22DAN+aGjII8p04RaxSXlxxcp55SY3Lh8mPfV8Kyb8mfnQdZ\nprN9Fes33NHboZpy9MQTcMDOlO8fyl6wI87r3FHRyJVYMFNhsfYfTdaKmc95ruLP\nTPrvG+Cr8ubw7ZUMF592PynVanyWwj1Mv2IQtAMPBr5aHfEFH6VQgAeImssfVXgd\n9PVz/rMYPmjbaKzbbNJFaIBbIvBKso8saqnia2Bw4UU1sXFYXK7o/R/JQFE6fvdz\nVqvKSYRpAgMBAAECgf8bp2vnJDQWyuxeY/HOsEFMvyTGn5fMN5PP6Z6UWNonXuaq\nR1//6N3Lm9Duu6hrSRYMPaq+jSmj9BbcspEVVECfkFZdBpXdKLWv5yvgRtlPjg4s\nYvRhh6DLjH9xnADZUF+MOk6LOnh43qwfraSoo6nOibXK4PaJl7lLI7CFAye7sWwX\nIqtIWE7WqEmJMXVXK56xRFE0nmZgBT074C6e+HvDFPcAnoFEMi3KDV7m6JJUDA0x\nfYl5yJpfdt2ansbims+ApVwZnst+R1LZbYllL1l3XCqo74GW0nix6EER3jLWLcJq\nRMXbJFLiTWCKdqX4sXEL7SBW8hRua9/0Djz+DoECgYEAyrgUnS7pCRrChYBv+Iau\nonNNurwIX9AkudtZq5ye5bsMJeeSk+OMgV9d1ao0jC3hmYQv7rWmIfe3oIAU0//5\niorkFXDkjv5sSRh0ZbTv9iyZm+HTc3AxC61Oq0jmap4bRhXGTtXSlzltn3mvDboN\n9n6yc8JiIO/L6y8ccooPj0ECgYEAtgVPbWjJ++xH3Ec31AjQ+d7M5J2l0mBgpkff\nxJx8QyG6ZegrGhPzYkVHhTosbzgMF4qqZTL8ykw1SiVWQ9JtRY9eo8Ip+CCHG8PS\ndsgQ2argp7EJXqSt3XAqbbsqiBUf3MvKCDjoz1YRQUOSjzfCy34toCE/jZ8KwKY6\nyeV50ykCgYA2Xbfpj7MfpfWVKCTfWf9rwdCOy+7TA1dWIOWjiJV0tv1DDR/aWaRM\n7e6UZtlA3u0p5rGUK1MquOyeNrtfP78E0MrqlSQ21TTlRZGx1RylwMBRdfbndqcw\nooH+ncZtwYk++of8jIu10sj7g/7Wh5dHDCN/uBnpC2Z85F2UukICAQKBgB5YeEt3\nQuVbGQfiX65b4esmd+ZNWg3ev345UVcH97eBcf/3Wug4F0V0412Uuz+3ilyKY6oH\nfBmF5wR7gQxBkrIcGt0WS8TwN1YrqXTgGd8QG/AZFcTPk6TtREWTBJ43V16KIx6H\nzlhysuG8kDyMQU+Hs7eUk3Ha6AICNJ7gQRUxAoGAGyxjjl5JTY2AltyvuLHMcTyY\nfN+VOsbzgLM4e43gGDJdOIACAEs2DCif16oLxn4ABTgYarMKUycatiJgtEG2zfRC\nhF/Tf+33dKQwZs++zMiwbFDLIbZmBn7lHHCIzgjsTvjjaz++eEjJXLCeiafY6VAG\nVERrcLIx4hyeoeISI/U=\n-----END PRIVATE KEY-----\n",
+  "client_email": "chriskapend@smartbrain-276214.iam.gserviceaccount.com",
+  "client_id": "101019067937392849937",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/chriskapend%40smartbrain-276214.iam.gserviceaccount.com"
+}
